24495cb09214ca5459b4a2bb6942221dffe96c2b
yatocham <ederjuninho2003@gmail.com>
2023-05-08 23:41:41 -0300

    Adicionando logout

--- src/components/header/dropdown/profile.jsx
+++ src/components/header/dropdown/profile.jsx
@@ -5,22 +5,17 @@ class DropdownProfile extends React.Component {
 		return (
 			<div className="navbar-item navbar-user dropdown">
 				<a href="#/" className="navbar-link dropdown-toggle d-flex align-items-center" data-bs-toggle="dropdown">
-					<img src="/assets/img/user/user-13.jpg" alt="" /> 
+					<img src="https://cdn.eutotal.com/imagens/frases-perfeitas-para-a-foto-de-perfil-cke.jpg" alt="Imagem de Perfil" /> 
 					<span>
-						<span className="d-none d-md-inline">Adam Schwartz</span>
+						<span className="d-none d-md-inline">Administrador</span>
 						<b className="caret"></b>
 					</span>
 				</a>
 				<div className="dropdown-menu dropdown-menu-end me-1">
-					<a href="#/" className="dropdown-item">Edit Profile</a>
-					<a href="#/" className="dropdown-item d-flex align-items-center">
-						Inbox
-						<span className="badge bg-danger rounded-pill ms-auto pb-4px">2</span> 
-					</a>
-					<a href="#/" className="dropdown-item">Calendar</a>
-					<a href="#/" className="dropdown-item">Settings</a>
+					<a href="#/" className="dropdown-item">Grupo de Acesso</a>
+					<a href="#/" className="dropdown-item">Configurações</a>
 					<div className="dropdown-divider"></div>
-					<a href="#/" className="dropdown-item">Log Out</a>
+					<a href="user" className="dropdown-item">Sair</a>
 				</div>
 			</div>
 		);

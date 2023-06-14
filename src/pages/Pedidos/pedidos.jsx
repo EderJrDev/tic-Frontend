6fe2b48b6bd9f53763607fb2a4b67482f7d87169
yatocham <ederjuninho2003@gmail.com>
2023-06-14 19:00:31 -0300

    Criando Lista de produtos para realizar um pedido

--- src/pages/Pedidos/pedidos.jsx
+++ src/pages/Pedidos/pedidos.jsx
@@ -3,81 +3,111 @@ import { Link } from 'react-router-dom';
 import { AppSettings } from './../../config/app-settings.js';
 import PerfectScrollbar from 'react-perfect-scrollbar';
 
-function CustomerOrder(props) {
+import { api } from "../../utils/api";
+import { Dialog } from 'primereact/dialog';
+import { InputText } from 'primereact/inputtext';
+
+
+
+function CustomerOrder() {
   const [posMobileSidebarToggled, setPosMobileSidebarToggled] = useState(false);
   const context = useContext(AppSettings);
 
-  // useEffect(() => {
-  //   context.handleSetAppSidebarNone(true);
-  //   context.handleSetAppHeaderNone(true);
-  //   context.handleSetAppContentFullHeight(true);
-  //   context.handleSetAppContentClass('p-0');
+  const [cli, setCli] = useState([]);
+  const [orders, setOrders] = useState([]);
+  const [selectedQuantityToAdd, setSelectedQuantityToAdd] = useState('');
+  const [showModal, setShowModal] = useState(false);
+  const [selectedCardQuantity, setSelectedCardQuantity] = useState('');
+  const [pedido, setPedido] = useState('');
+  const [selectedProduct, setSelectedProduct] = useState(null); // novo estado para armazenar o produto selecionado
 
-  //   return () => {
-  //     context.handleSetAppSidebarNone(false);
-  //     context.handleSetAppHeaderNone(false);
-  //     context.handleSetAppContentFullHeight(false);
-  //     context.handleSetAppContentClass('');
-  //   };
-  // }, []);
-
-  const togglePosMobileSidebar = () => {
-    setPosMobileSidebarToggled(!posMobileSidebarToggled);
+  const handleCardClick = (item) => {
+    setSelectedCardQuantity(item.quantity);
+    setShowModal(true);
+    setSelectedProduct(item); // armazena o produto selecionado no estado
   };
+
+  const updateProduct = () => {
+    const updatedOrders = [...orders];
+    updatedOrders.push({
+      product: selectedProduct.name,
+      quantity: selectedQuantityToAdd
+    });
+    setOrders(updatedOrders);
+  };
+  
+
+
+  useEffect(() => {
+    async function getCategory() {
+      const response = await api.get("/admin/product");
+      const dados = response.data
+      setCli(dados);
+    }
+    getCategory();
+  }, []);
 
   return (
     <div className="vh-100">
-      <div className={'pos pos-customer ' + (posMobileSidebarToggled ? 'pos-mobile-sidebar-toggled' : '')} id="pos-customer">
-        {/* <div className="pos-menu">
-          <div className="logo">
-            <Link to="/">
-              <div className="logo-text"><i class="bi bi-arrow-left-circle"></i> Voltar</div>
-            </Link>
-          </div>
-          <div className="nav-container">
-            <PerfectScrollbar className="height-full" options={{ suppressScrollX: true }}>
-              <ul className="nav nav-tabs">
-                <li className="nav-item">
-                  <Link to="/pos/customer-order" className="nav-link active" data-filter="all">
-                    <i className="fa fa-fw fa-utensils mr-1 ml-n2"></i> Cozinha
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link to="/pos/customer-order" className="nav-link" data-filter="burger">
-                    <i className="fa fa-fw fa-hamburger mr-1 ml-n2"></i> Escritório
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link to="/pos/customer-order" className="nav-link" data-filter="pizza">
-                    <i className="fa fa-fw fa-pizza-slice mr-1 ml-n2"></i> Limpeza
-                  </Link>
-                </li>
-              </ul>
-            </PerfectScrollbar>
-          </div>
-        </div> */}
+      <div className={`pos pos-customer ${posMobileSidebarToggled ? 'pos-mobile-sidebar-toggled' : ''}`} id="pos-customer">
         <div className="pos-content">
           <PerfectScrollbar className="pos-content-container" options={{ suppressScrollX: true }}>
             <div className="product-row">
               <div className='col-lg-12'>
                 <h1 className="page-header">Itens</h1>
               </div>
-              <div className='col-lg-4 pb-3'>
-                <div className="container" data-type="meat">
-                  <Link to="/pos/customer-order" className="product" data-bs-toggle="modal" data-bs-target="#modalPosItem">
-                    <div className="text">
-                      <div className="title">Arrroz</div>
-                      <div className="desc">Categoria: Alimento</div>
-                      <div className="price">Quantidade: 12</div>
-                      <div className="desc">Status: Aguardando</div>
-                    </div>
-                  </Link>
+              {cli.map(item => (
+                <div className='col-lg-4 pb-3' key={item.id}>
+                  <div className="container" data-type="meat">
+                    <Link className="product" data-bs-target="#" onClick={() => handleCardClick(item)}>
+                      <div className="text">
+                        <div className="title">{item.name}</div>
+                        <div className="desc">Categoria: {item.location}</div>
+                        <div className="price">Quantidade: {item.quantity}</div>
+                      </div>
+                    </Link>
+                  </div>
+                </div>
+              ))}
+            </div>
+            <Dialog
+              modal
+              maximizable
+              header="Editar Produtos"
+              visible={showModal}
+              onHide={() => setShowModal(false)}
+              style={{ width: '75vw' }}
+              contentStyle={{ height: '300px' }}
+            >
+              <div className="row mt-5 m-auto">
+                <div className='col-lg-2'>
+                  <p className='m-auto pb-2'>Quantidade em Estoque:</p>
+                  <InputText
+                    type="text"
+                    className="p-inputtext-sm w-100"
+                    value={selectedCardQuantity}
+                  />
+                </div>
+                <div className='col-lg-2'>
+                  <p className='m-auto pb-2'>Quantidade a Ser Adicionada:</p>
+                  <InputText
+                    type="text"
+                    className="p-inputtext-sm w-100"
+                    value={selectedQuantityToAdd}
+                    onChange={(e) => setSelectedQuantityToAdd(e.target.value)}
+                  />
+                </div>
+                <div className='col-lg-2'>
+                  <div className='pt-4 mt-3'>
+                    <button className="btn btn-info btn-btn-sm" onClick={updateProduct}>
+                      <i className="bi bi-check-circle-fill"></i> Atualizar
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
+            </Dialog>
           </PerfectScrollbar>
         </div>
-
         <div className="pos-sidebar" id="pos-sidebar">
           <div className="pos-sidebar-nav">
             <ul className="nav nav-tabs nav-fill">
@@ -89,52 +119,19 @@ function CustomerOrder(props) {
           <div className="pos-sidebar-body tab-content" data-scrollbar="true" data-height="100%">
             <div className="tab-pane fade h-100 show active" id="newOrderTab">
               <div className="pos-table">
-                <div className="row pos-table-row">
-                  <div className="col-9">
-                    <div className="pos-product-thumb">
-                      <div className="img" style={{ backgroundImage: 'url(/assets/img/pos/product-2.jpg)' }}></div>
-                      <div className="info">
-                        <div className="title">Arroz</div>
+                {orders.map((order, index) => (
+                  <div className="row pos-table-row" key={index}>
+                    <div className="col-9">
+                      <div className="pos-product-thumb">
+                        <div className="info">
+                          <div className="title">{order.product}</div>
+                        </div>
                       </div>
                     </div>
+                    <div className="col-3 total-price">Qtd: {order.quantity}</div>
                   </div>
-                  <div className="col-3 total-price">Qtd: 12</div>
-                </div>
-                <div className="row pos-table-row">
-                  <div className="col-9">
-                    <div className="pos-product-thumb">
-                      <div className="img" style={{ backgroundImage: 'url(/assets/img/pos/product-2.jpg)' }}></div>
-                      <div className="info">
-                        <div className="title">Arroz</div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-3 total-price">Qtd: 12</div>
-                </div>
-                <div className="row pos-table-row">
-                  <div className="col-9">
-                    <div className="pos-product-thumb">
-                      <div className="img" style={{ backgroundImage: 'url(/assets/img/pos/product-2.jpg)' }}></div>
-                      <div className="info">
-                        <div className="title">Arroz</div>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-3 total-price">Qtd: 12</div>
-                </div>
-              </div>
-            </div>
-            <div className="tab-pane fade h-100" id="orderHistoryTab">
-              <div className="h-100 d-flex align-items-center justify-content-center text-center p-20">
-                <div>
-                  <div className="mb-3 mt-n5">
-                    <svg width="6em" height="6em" viewBox="0 0 16 16" className="text-gray-300" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                      <path fillRule="evenodd" d="M14 5H2v9a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V5zM1 4v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4H1z" />
-                      <path d="M8 1.5A2.5 2.5 0 0 0 5.5 4h-1a3.5 3.5 0 1 1 7 0h-1A2.5 2.5 0 0 0 8 1.5z" />
-                    </svg>
-                  </div>
-                  <h4>No order history found</h4>
-                </div>
+                ))}
+
               </div>
             </div>
           </div>
@@ -148,118 +145,10 @@ function CustomerOrder(props) {
               <div className="price">5</div>
             </div>
             <div className="btn-row">
-              {/* <Link to="/pos/customer-order" className="btn btn-default"><i className="fa fa-bell fa-fw fa-lg"></i> Service</Link>
-              <Link to="/pos/customer-order" className="btn btn-default"><i className="fa fa-file-invoice-dollar fa-fw fa-lg"></i> Bill</Link> */}
               <Link to="/pos/customer-order" className="btn btn-success"><i className="fa fa-check fa-fw fa-lg"></i> Finalizar Pedido</Link>
             </div>
           </div>
-
-
         </div>
-
-        <Link to="/pos/customer-order" className="pos-mobile-sidebar-toggler" onClick={() => this.togglePosMobileSidebar()}>
-          <svg viewBox="0 0 16 16" className="img" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-            <path fillRule="evenodd" d="M14 5H2v9a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V5zM1 4v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4H1z" />
-            <path d="M8 1.5A2.5 2.5 0 0 0 5.5 4h-1a3.5 3.5 0 1 1 7 0h-1A2.5 2.5 0 0 0 8 1.5z" />
-          </svg>
-          <span className="badge">5</span>
-        </Link>
-
-        <div className="modal modal-pos-item fade" id="modalPosItem">
-          <div className="modal-dialog modal-lg">
-            <div className="modal-content">
-              <div className="modal-body p-0">
-                <button data-bs-dismiss="modal" className="btn-close position-absolute top-0 end-0 m-4"> </button>
-                <div className="pos-product">
-                  <div className="pos-product-img">
-                    <div className="img" style={{ backgroundImage: 'url(/assets/img/pos/product-1.jpg)' }}></div>
-                  </div>
-                  <div className="pos-product-info">
-                    <div className="title">Grill Chicken Chop</div>
-                    <div className="desc">
-                      chicken, egg, mushroom, salad
-                    </div>
-                    <div className="price">$10.99</div>
-                    <hr />
-                    <div className="option-row">
-                      <div className="qty">
-                        <div className="input-group">
-                          <a href="#/" className="btn btn-default"><i className="fa fa-minus"></i></a>
-                          <input type="text" className="form-control border-0 text-center" name="" defaultValue="1" />
-                          <a href="#/" className="btn btn-default"><i className="fa fa-plus"></i></a>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="option-row">
-                      <div className="option-title">Size</div>
-                      <div className="option-list">
-                        <div className="option">
-                          <input type="radio" id="size3" name="size" className="option-input" defaultChecked />
-                          <label className="option-label" htmlFor="size3">
-                            <span className="option-text">Small</span>
-                            <span className="option-price">+0.00</span>
-                          </label>
-                        </div>
-                        <div className="option">
-                          <input type="radio" id="size1" name="size" className="option-input" />
-                          <label className="option-label" htmlFor="size1">
-                            <span className="option-text">Large</span>
-                            <span className="option-price">+3.00</span>
-                          </label>
-                        </div>
-                        <div className="option">
-                          <input type="radio" id="size2" name="size" className="option-input" />
-                          <label className="option-label" htmlFor="size2">
-                            <span className="option-text">Medium</span>
-                            <span className="option-price">+1.50</span>
-                          </label>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="option-row">
-                      <div className="option-title">Add On</div>
-                      <div className="option-list">
-                        <div className="option">
-                          <input type="checkbox" name="addon[sos]" value="true" className="option-input" id="addon1" />
-                          <label className="option-label" htmlFor="addon1">
-                            <span className="option-text">More BBQ sos</span>
-                            <span className="option-price">+0.00</span>
-                          </label>
-                        </div>
-                        <div className="option">
-                          <input type="checkbox" name="addon[ff]" value="true" className="option-input" id="addon2" />
-                          <label className="option-label" htmlFor="addon2">
-                            <span className="option-text">Extra french fries</span>
-                            <span className="option-price">+1.00</span>
-                          </label>
-                        </div>
-                        <div className="option">
-                          <input type="checkbox" name="addon[ms]" value="true" className="option-input" id="addon3" />
-                          <label className="option-label" htmlFor="addon3">
-                            <span className="option-text">Mushroom soup</span>
-                            <span className="option-price">+3.50</span>
-                          </label>
-                        </div>
-                        <div className="option">
-                          <input type="checkbox" name="addon[ms]" value="true" className="option-input" id="addon4" />
-                          <label className="option-label" htmlFor="addon4">
-                            <span className="option-text">Lemon Juice (set)</span>
-                            <span className="option-price">+2.50</span>
-                          </label>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="btn-row">
-                      <button className="btn btn-default" data-bs-dismiss="modal">Cancel</button>
-                      <button className="btn btn-success">Add to cart <i className="fa fa-plus fa-fw ms-2"></i></button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
       </div>
     </div>
   );

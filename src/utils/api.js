69d578ccc8de8f784b86db046969f35d58e1c82f
Eder Jr <ederjuninho2003@gmail.com>
2023-11-23 09:15:41 -0300

    fix: change endpoint

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 let api = axios.create({
-   //baseURL: 'http://localhost:1234',
+  //baseURL: 'http://localhost:1234',
   baseURL: "https://tic-estoque-v2.vercel.app/",
   timeout: 10000,
 });
